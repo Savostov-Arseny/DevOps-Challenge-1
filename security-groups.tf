ae79286f813c5392bec366fb32f4adb26893dcff
Savostov-Arseny <savostov.arseny@gmail.com>
2019-12-11 16:39:07 +0400

    remove 443 inbound rule, & ICMP

--- security-groups.tf
+++ security-groups.tf
@@ -1,6 +1,6 @@
 #Security group that will be attached to host
 resource "aws_security_group" "bastion-host" {
-  name = "SSH+HTTPS+ICMP"
+  name = "SSH"
   # Allow SSH
   ingress {
     from_port   = 22
@@ -15,26 +15,12 @@ resource "aws_security_group" "bastion-host" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   # Allow https for aws cli
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   egress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  # Enable ICMP
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = var.tag_name
   }
